479d70b33d9dbf32c2a6eba975ff4891b5c3630a
dujianwu123 <dujianwu123@126.com>
2019-09-15 23:07:56 +0800

    首页后台开发

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 import Koa from 'koa'
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
-
+import mongoose from 'mongoose'
+import bodyParser from 'koa-bodyparser'
+import session from 'koa-generic-session'
+import Redis from 'koa-redis'
+import json from 'koa-json'
+import dbConfig from './dbs/config'
+import passport from './interface/utils/passport'
+import users from './interface/users'
 const app = new Koa()
 
 // Import and Set Nuxt.js options
@@ -16,7 +23,20 @@ async function start() {
     host = process.env.HOST || '127.0.0.1',
     port = process.env.PORT || 3000
   } = nuxt.options.server
-
+  // 设置session 的名字和签名
+  app.keys = ['mt', 'keyskeys']
+  app.proxy = true
+  app.use(session({key: 'mt', prefix: 'mt:uid', store: new Redis()}))
+  app.use(bodyParser({
+    extendTypes:['json','form','text']
+  }))
+  app.use(json())
+  
+  mongoose.connect(dbConfig.dbs,{
+    useNewUrlParser:true
+  })
+  app.use(passport.initialize())
+  app.use(passport.session())
   // Build in development
   if (config.dev) {
     const builder = new Builder(nuxt)
@@ -24,7 +44,7 @@ async function start() {
   } else {
     await nuxt.ready()
   }
-
+  app.use(users.routes()).use(users.allowedMethods())
   app.use((ctx) => {
     ctx.status = 200
     ctx.respond = false // Bypass Koa's built-in response handling
